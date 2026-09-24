9605c185e85f910c4147548c145554592d92aa23
ramesh1985tr-crypto <ramesh1985tr@gmail.com>
2026-09-24 16:16:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE = 'formula-pro-v2-2';
+const CACHE = 'formula-pro-v2-4';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
